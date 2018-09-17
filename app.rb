9df659c1ce24c82472c96f9d14c64bff57304a87
NadiaAiraf <aidanfaria@gmail.com>
2018-09-17 16:40:21 +0100

    we created a cat with red border lines

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
 
+# set :session_secret, 'super secret'
+
 get '/' do
   "Emma's laptop is broken :("
+end
+
+get '/secret' do
+  "Emma's laptop will RISE AGAAAAAAAAAAAAAIN!!!!!"
+end
+
+get '/makers' do
+  "We are AMAZING!!!!"
+end
+
+get '/cohort/september' do
+  "Is the be-e-eeeee-SSSSSSSSS.T"
+end
+
+get '/cat' do
+
+  "<div>
+  <img src='http://bit.ly/1eze8aE' alt='a picture of a cat' style='border: 5px dashed red'>
+  </div>"
 end
